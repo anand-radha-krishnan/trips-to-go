346aed7f2b71187b9d792a0367a413c26c888be8
Anand Radha Krishnan <85664565+anand-radha-krishnan@users.noreply.github.com>
2025-02-20 11:21:59 +0100

    tryout deployment

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,8 @@
-const path = require('path');
-
 module.exports = {
   entry: './public/js/index.js',
   devtool: 'inline-source-map',
   output: {
     filename: 'main.js',
-    path: path.resolve(__dirname, 'public/js'),
+    path: './',
   },
 };
