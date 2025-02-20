cf0989215a5f60a54c5d1cc5047c03fb503be2ee
Anand Radha Krishnan <anandradhakrishna@outlook.com>
2025-02-20 13:06:44 +0100

    fix trust proxy

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,10 @@
+const path = require('path');
+
 module.exports = {
   entry: './public/js/index.js',
   devtool: 'inline-source-map',
   output: {
-    filename: 'main.js'
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'public/js'),
   },
 };
