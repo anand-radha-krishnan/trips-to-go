8fce3faf098fa24ed6e9d323dd00416032410cf2
Anand Radha Krishnan <anandradhakrishna@outlook.com>
2025-02-20 13:02:32 +0100

    fix trust proxy

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ const errorHandler = require('./controllers/errorController');
 
 const app = express();
 // Trust proxy
-app.enable('trust proxy');
+app.enable('trust proxy', 1);
 
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
@@ -41,6 +41,8 @@ const limiter = rateLimit({
   max: 100,
   windowMs: 60 * 60 * 1000,
   message: 'Too many requests from the IP, please try again later',
+  standardHeaders: true,
+  legacyHeaders: false,
 });
 app.use('/api', limiter);
 
